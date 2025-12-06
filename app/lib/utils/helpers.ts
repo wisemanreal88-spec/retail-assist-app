28a7773d53ee7ed47e38f53c7024ab771f8875a7
agritracker7-netizen <agritracker7@gmail.com>
2025-12-06 19:03:50 +0000

    Update generateApiKey to accept optional length parameter

--- app/lib/utils/helpers.ts
+++ app/lib/utils/helpers.ts
@@ -3,6 +3,11 @@ export function maskApiKey(apiKey: string): string {
   return apiKey.substring(0, 4) + '***' + apiKey.substring(apiKey.length - 4);
 }
 
-export function generateApiKey(): string {
-  return 'sk_' + Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+export function generateApiKey(length: number = 32): string {
+  const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let result = 'sk_';
+  for (let i = 0; i < length; i++) {
+    result += chars.charAt(Math.floor(Math.random() * chars.length));
+  }
+  return result;
 }
